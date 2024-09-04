795ce9cf8e2beae69b435b5399b40c8baf9cff79
AimanHafizi619 <120614793+AimanHafizi619@users.noreply.github.com>
2024-09-04 16:15:31 +0800

    Practice Unit Test
    Do Exercise

--- test_string.py
+++ test_string.py
@@ -50,5 +50,63 @@ class TestNoneValue(unittest.TestCase):
         message = "Test value is not None"
         self.assertIsNone(secondValue, message)
 
+class TestInstance(unittest.TestCase):
+
+    def test_instance(self):
+        self.assertIsInstance(1, int)
+        self.assertIsInstance("hello", str)
+        self.assertIsInstance(1.0, float)
+
+    def test_not_instance(self):
+        self.assertNotIsInstance(1, str)
+        self.assertNotIsInstance("hello", int)
+        self.assertNotIsInstance(1.0, str)
+
+class TestClass:
+    x = 5
+
+class TestClass_minor:
+    x = 6
+
+class TestObjectInstance(unittest.TestCase):
+
+    def test_object_isInstance(self):
+        objectName = TestClass()
+        message = "Given object is an instance of TestClass."
+        self.assertIsInstance(objectName, TestClass, message)
+
+    def test_object_notIsInstance(self):
+        objectName = TestClass()
+        message = "Given object is not an instance of TestClass."
+        self.assertNotIsInstance(objectName, TestClass_minor, message)
+
+class TestNotAlmostEqual(unittest.TestCase):
+    
+    def test_count_decimalPlace(self):
+        first_num = 4.4555
+        second_num = 4.4566
+
+        decimalPlace_1 = 2
+        decimalPlace_2 = 3
+
+        message1 = "first and second are almost equal."
+        message2 = "first and second are almost not equal."
+
+        self.assertAlmostEqual(first_num, second_num, decimalPlace_1, message1)
+        self.assertNotAlmostEqual(first_num, second_num, decimalPlace_2, message2)
+
+    def test_count_deltaPlace(self):
+        first_num = 4.4555
+        second_num = 4.4566
+
+        delta1 = 0.002
+        delta2 = 0.0001
+
+        message1 = "first and second are almost equal."
+        message2 = "first and second are almost not equal."
+
+        self.assertAlmostEqual(first_num, second_num, None, message1, delta1)
+        self.assertNotAlmostEqual(first_num, second_num, None, message2, delta2)
+
 if __name__ == '__main__':
     unittest.main()
