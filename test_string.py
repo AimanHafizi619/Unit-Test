b6ec3ddf9826f2fab8b88e3b96eb14db13f5843e
AimanHafizi619 <120614793+AimanHafizi619@users.noreply.github.com>
2024-09-04 14:25:14 +0800

    Practice Test Case
    Do Exercise

--- test_string.py
+++ test_string.py
@@ -22,6 +22,33 @@ class TestString(unittest.TestCase):
         with self.assertRaises(TypeError):
             word.split(2)  
 
+class TestStringMethod(unittest.TestCase):
+
+    def test_neg_string(self):
+        key = "love"
+        container = "clover"
+        # Error message in case test case failed
+        message = "The key is not in the container"
+        self.assertIn(key, container, message)
+
+    def test_pos_string(self):
+        key = "clvr"
+        container = "clover"
+        # Error message in case test case failed
+        message = "The key is in the container"
+        self.assertNotIn(key, container, message)
+
+class TestNoneValue(unittest.TestCase):
+
+    def test_not_none_value(self):
+        firstValue = 2024
+        message = "Test value is None."
+        self.assertIsNotNone(firstValue, message)
+
+    def test_none_value(self):
+        secondValue = None
+        message = "Test value is not None"
+        self.assertIsNone(secondValue, message)
+
 if __name__ == '__main__':
     unittest.main()
-    
