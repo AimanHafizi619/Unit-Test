035dbaa3e1bf91974a893b6ea4a80ca800ea49c7
AimanHafizi619 <120614793+AimanHafizi619@users.noreply.github.com>
2024-09-04 11:34:40 +0800

    Practice Unit Test
    Do exercise

--- test_prime.py
+++ test_prime.py
@@ -1,37 +1,54 @@
 import unittest
 
 from prime_number import is_prime
-
-# class TestClass(unittest.TestCase):
-#     def test_prime_not_prime(self):
-#         self.assertTrue(is_prime(2))
-#         self.assertTrue(is_prime(5))
-#         self.assertTrue(is_prime(9))
-#         self.assertTrue(is_prime(11))
+from add_number import add
 
 class TestPrime(unittest.TestCase):
 
-    def test_1(self):
+    def test_prime_not_prime(self):
         self.assertTrue(is_prime(2))
-    def test_2(self):
         self.assertTrue(is_prime(5))
-    def test_3(self):
         self.assertFalse(is_prime(9))
-    def test_4(self):
         self.assertTrue(is_prime(11))
 
+    # def test_1(self):
+    #     self.assertTrue(is_prime(2))
+    # def test_2(self):
+    #     self.assertTrue(is_prime(5))
+    # def test_3(self):
+    #     self.assertFalse(is_prime(9))
+    # def test_4(self):
+    #     self.assertTrue(is_prime(11))
 
-    def test_type_error1(self):
-        with self.assertRaises(TypeError):
-            is_prime("five")
+    def test_exception(self):
+        self.assertRaises(TypeError,is_prime,"five")
+        self.assertRaises(TypeError,is_prime,7.0)
+        self.assertRaises(TypeError,is_prime,"five")
 
-    def test_type_error2(self):
-        with self.assertRaises(TypeError):
-            is_prime(7.0)
+    # def test_type_error1(self):
+    #     with self.assertRaises(TypeError):
+    #         is_prime("five")
+    # def test_type_error2(self):
+    #     with self.assertRaises(TypeError):
+    #         is_prime(7.0)
+    # def test_value_error(self):
+    #     with self.assertRaises(ValueError):
+    #         is_prime(-11)
 
-    def test_value_error(self):
-        with self.assertRaises(ValueError):
-            is_prime(-11)
+    def test_add_function(self):
+        self.assertEqual(add(1, 2), 3)
+        self.assertEqual(add(-1, -2), -3)
+        self.assertEqual(add(0, 0), 0)
+        self.assertEqual(add(-1, 2), 1)
+
+    # def test_add_pos_num(self):
+    #     self.assertEqual(add(1, 2), 3)
+    # def test_add_neg_num(self):
+    #     self.assertEqual(add(-1, -2), -3)
+    # def test_add_zero(self):
+    #     self.assertEqual(add(0, 0), 0)
+    # def test_mix_num(self):
+    #     self.assertEqual(add(-1, 2), 1)
 
 if __name__ == '__main__':
     unittest.main()
